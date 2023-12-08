f24997a6a2f3333986921518292aa0a8cc6bf7e1
nhiluu <nhi.luu@team.enouvo.com>
2023-12-08 13:52:22 +0700

    Initial Commit

---
+++ FinTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinTech
+//
+//  Created by nhi.luu on 08/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
